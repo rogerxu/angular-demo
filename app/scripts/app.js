d5bacd771bf2b4b9c5e021ea3925d4e759b3fd8e
Roger Xu <roger.xu.2007@gmail.com>
2014-01-25 01:39:21 +0800

    Refine js code.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,20 @@
 'use strict';
 
-angular.module('angularDemoApp', [
-        'ngCookies',
-        'ngResource',
-        'ngSanitize',
-        'ngRoute'
-    ])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+var app = angular.module('app', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'ui'
+]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,9 @@
 'use strict';
 
-angular.module('angularDemoApp')
-    .controller('MainCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
-    });
+angular.module('app').controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+    ];
+});
